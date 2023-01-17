d493b89a9f5c849110bd3d3c300a74c58d799d16
Franckie Andria <franckieandriamalala@gmail.com>
2023-01-17 16:03:24 +0300

    Send Mail

--- service/mailService.js
+++ service/mailService.js
@@ -1,5 +1,25 @@
+const nodemailer = require('nodemailer') ;
+
+const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com" ,
+    port: 465 , 
+    secure: true ,
+    auth: {
+        user: "franckieandriamalala.itu@gmail.com" ,
+        pass: "dlfaovbimykouesl"
+    }
+}) ;
+
 const sendMail = async (sendTo, contenu) => {
-    console.log('We are going to send Mail to => '+sendTo+' <= the content : '+contenu) ;
+    transporter.sendMail({
+        from: "reparecarmada@gmail.com" ,
+        to: sendTo ,
+        subject: "REPARE CAR Mada" ,
+        text: contenu
+    }, function (err, info) {
+        if (err) console.log(err) ;
+        else console.log('We are going to send Mail to => '+sendTo+' <= the content : '+contenu) ;
+    }) ;
 }
 
 module.exports = {
